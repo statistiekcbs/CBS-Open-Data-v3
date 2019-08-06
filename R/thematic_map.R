6c706501ca8a256312aed6408c4417f20f6339e0
Jolien Oomens <jj.oomens@cbs.nl>
2019-08-06 08:56:27 +0200

    Weghalen overbodige kolom thematische kaart

--- R/thematic_map.R
+++ R/thematic_map.R
@@ -16,8 +16,8 @@ metadata <- cbs_get_meta("83765NED")
 print(metadata$DataProperties$Key)
 
 # Haal alle geboortecijfers op
-data <- cbs_get_data("83765NED", select=c("WijkenEnBuurten","Codering_3","GeboorteRelatief_25")) %>%
-  mutate(Codering_3 = str_trim(Codering_3))
+data <- cbs_get_data("83765NED", select=c("WijkenEnBuurten","GeboorteRelatief_25")) %>%
+  mutate(WijkenEnBuurten = str_trim(WijkenEnBuurten))
 
 # De geodata wordt via de API van het Nationaal Georegister van PDOK opgehaald.
 # Een overzicht van beschikbare data staat op https://www.pdok.nl/datasets.
@@ -27,7 +27,7 @@ download.file(geoUrl, fileName)
 gemeentegrenzen <- geojson_read(fileName, what = "sp")
 
 gemeentegrenzen@data <- gemeentegrenzen@data %>% 
-  left_join(data,by=c("statcode"="Codering_3"))
+  left_join(data,by=c("statcode"="WijkenEnBuurten"))
 
 g <- fortify(gemeentegrenzen, region = "id")
 g <- merge(g, gemeentegrenzen@data, by = "id")
